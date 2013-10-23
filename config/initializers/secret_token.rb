ae955edc278ed7112073cc2363a04b09baa8f8c5
Rafael <cotifael@gmail.com>
2013-10-22 22:59:25 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D6Project::Application.config.secret_key_base = 'f16d84393e4cd57d3aac373a433181f9a0de375cee7af021b0b0c971b5ba8d008422c43b864706a5db377fd804a69187e953b992e84f9344c4ffc4144ba22a8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D6Project::Application.config.session_store :cookie_store, key: '_d6_project_session'
