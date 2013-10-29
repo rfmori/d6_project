246289106b7531b99cb96124cd68d986d942badb
tiotti <meumesmomyself@gmail.com>
2013-10-29 02:16:39 -0300

    Utilizando stored procedures no model de user e pequeno controller de teste.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   self.table_name = 'tb_user'
 
+
+  def self.create_user(name)
+    connection.execute("SELECT create_user('#{name}')")
+  end
+
+  def self.find_user_by_name(name)
+    r = connection.select_all("select find_user_by_name('#{name}')")
+    r.rows
+  end
+
 end
