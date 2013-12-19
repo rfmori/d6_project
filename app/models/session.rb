62efdf632398e9e75e8fac63cebab866f4f376c4
tiotti <meumesmomyself@gmail.com>
2013-12-19 11:50:20 -0300

    Stored Procedures para criação e abertura de sessão

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,13 @@ class Session < ActiveRecord::Base
   self.table_name = 'tb_session'
   has_one :table, :foreign_key => 'mesa_id'
   has_many :posts
+
+  def create_session(table_id,is_dynamic,session_name)
+    r = connection.select_all("SELECT create_session('#{session_name}',#{table_id},#{is_dynamic})")
+    r
+  end
+
+  def open_session()
+    connection.execute("SELECT open_session('#{self['id']}')")
+  end
 end
