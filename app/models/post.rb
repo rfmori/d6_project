9e8238d08140ff13e8cc70efeaaa6fb9de7ffbde
tiotti <meumesmomyself@gmail.com>
2014-01-06 11:20:29 -0300

    Criação de mensagens

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   self.table_name = 'tb_post'
   belongs_to :user, :foreign_key => 'user_id'
   has_one :session, :foreign_key => 'sessao_id'
+
+  def post_message(ses_id, plr_id,message,status)
+    r = connection.select_all("SELECT post_message('#{ses_id}',#{plr_id},#{message},#{status})")
+    r
+  end
 end
